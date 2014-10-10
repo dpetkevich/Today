6736efc433897799823701b86ffcf3b05564f481
Daniel Petkevich <danielpetkevich@Daniels-MacBook-Pro.local>
2014-10-09 22:19:20 -0700

    Initial Commit

---
+++ Today/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Today
+//
+//  Created by Daniel Petkevich on 10/9/14.
+//  Copyright (c) 2014 Daniel Petkevich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
